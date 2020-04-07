c21795d30391cc2eba91dc63ce22050fdc32396e
mohannautiyal <mohannautiyal80@gmail.com>
2020-04-07 23:21:52 +0530

    Report changes

--- cucumberProject/src/main/java/com/home/stepDefinition.java
+++ cucumberProject/src/main/java/com/home/stepDefinition.java
@@ -1,20 +1,55 @@
 package com.home;
 
+import static org.junit.Assert.assertTrue;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 public class stepDefinition {
 	
+	int num1,num2;
+	
+	@Before
+	public void RunBefore() {
+		System.out.println("==============================");;
+		System.out.println("Run this before each scenario");
+		
+	}
+	
+	
+	@After
+	public void RunAfter() {
+		System.out.println("Run this ater each scenario");
+		System.out.println("==============================");;
+		
+	}
+	
+	
+	
+	/*
+	 * @Before("@subs, @add") public void RunBeforeT() {
+	 * System.out.println("==============================");;
+	 * System.out.println("Run this before add and subs scenario");
+	 * 
+	 * }
+	 */
 	
 		
-	@Given("user provides two numbers {string} and {string}")
-	public void user_provides_two_numbers_and(String string, String string2) {
+	@Given("user provides two numbers {int} and {int}")
+	public void user_provides_two_numbers_and(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
 	    System.out.println("two numbers provided here");
 	}
 
-	@Then("the sum of the two numbers should be {string}")
-	public void the_sum_of_the_two_numbers_should_be(String string) {
+	@Then("the sum of the two numbers should be {int}")
+	public void the_sum_of_the_two_numbers_should_be(int sum) {
+		
 	    System.out.println("Sum is shown here");
+
+		assertTrue("Sum is incorrect", sum == (num1 + num2));
 		
 		
 	}
--- cucumberProject/target/test-classes/Features/arithmethicOperations.feature
+++ cucumberProject/target/test-classes/Features/arithmethicOperations.feature
@@ -1,5 +1,11 @@
 Feature: Airthmethic Operations
 
+@add
 Scenario: Addition of two numbers
-Given user provides two numbers "3" and "5"
-Then the sum of the two numbers should be "8"
+Given user provides two numbers 3 and 5
+Then the sum of the two numbers should be 10
+
+@subs
+Scenario: Substraction of two numbers
+Given user provides two numbers 10 and 5
+Then the sum of the two numbers should be 15
